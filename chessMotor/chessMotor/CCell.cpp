56a9e4d425622615404af9a886d6b4a7f007a1f5
catsymptote <catsymptote@gmail.com>
2018-05-14 15:25:50 +0200

    Initial classes

---
+++ chessMotor/chessMotor/CCell.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "CCell.h"
+
+
+CCell::CCell()
+{
+}
+
+
+CCell::~CCell()
+{
+}
---
+++ chessMotor/chessMotor/CPiece.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "CPiece.h"
+
+
+CPiece::CPiece()
+{
+}
+
+
+CPiece::~CPiece()
+{
+}
